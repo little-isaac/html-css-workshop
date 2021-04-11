af74155533c3fb5f1594e2f0b5b6132b7a7dbc83
Milind Patel <ma353353@gmail.com>
2021-04-11 14:39:58 +0530

    =hide show code addede

--- public_html/javascript/js/index.js
+++ public_html/javascript/js/index.js
@@ -11,3 +11,18 @@ function addition(){
     document.querySelector(".result").innerHTML = added;
     document.querySelector(".result-field").value = added;
 }
+
+function toggleElement() {
+    var hideElement = document.querySelector('.hide-element');
+    var elemClassList = document.querySelector('.hide-element').classList;
+    
+    if (elemClassList.contains("hidden")) {
+        document.querySelector('.hide-element').classList.remove("hidden");
+    }else{
+        document.querySelector('.hide-element').classList.add("hidden");
+    }
+}
+
+var toggleButton = document.querySelector('.toggle-button');
+
+toggleButton.addEventListener("click", toggleElement);
